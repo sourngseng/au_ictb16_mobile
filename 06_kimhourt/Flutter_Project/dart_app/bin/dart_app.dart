86fc4c39117a161004f5a836c6b1eb2844121350
Kimhourt <tata1562583@gmail.com>
2022-12-20 23:03:58 +0700

    Upload data_app

---
+++ 06_kimhourt/Flutter_Project/dart_app/bin/dart_app.dart
@@ -0,0 +1,8 @@
+import 'package:dart_app/dart_app.dart' as dart_app;
+import 'package:dart_app/test_sum.dart' as test_sum;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_app.calculate()}!');
+
+  print("Result: ${test_sum.sum()}! ");
+}
