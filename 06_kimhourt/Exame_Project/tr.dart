b4eb6d6be5aea15bb2813b4675168f94bba97e14
Kimhourt <tata1562583@gmail.com>
2023-01-10 10:59:05 +0700

    Update

---
+++ 06_kimhourt/Exame_Project/tr.dart
@@ -0,0 +1,15 @@
+void main(List<String> args) {
+  double age = 24;
+
+  age += 1; //Here age +=1 mean age = age + 1;
+  print(age);
+
+  age -= 1; //Here age -=1 mean age = age - 1;
+  print(age);
+
+  age *= 2; //Here age *=2 mean age = age * 2;
+  print(age);
+
+  age /= 2; //Here age *=2 mean age = age * 2;
+  print(age);
+}
