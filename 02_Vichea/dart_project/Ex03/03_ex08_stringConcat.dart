4f0fc1f8e8c0cd829e7ca312666587a51db10eb2
VicheaUnlimited <kimsreymommak@gmail.com>
2023-01-12 20:50:36 +0700

    Ex03

---
+++ 02_Vichea/dart_project/Ex03/03_ex08_stringConcat.dart
@@ -0,0 +1,6 @@
+void main(List<String> args) {
+  String firstName = "Vichea";
+  String lastName = "Unlimited";
+  print("Using + Full Name is " + firstName + "" + lastName + ".");
+  print("Using interpolation, fulname is $firstName $lastName.");
+}
