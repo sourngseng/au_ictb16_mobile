1062a95e51f18b33a03e7ec090867649d04eb564
maopanharith <120468438+maopanharith@users.noreply.github.com>
2023-01-12 19:03:38 +0700

    Add files via upload

---
+++ 07_panharith/Test/Mao Panharith Lession1/ex03_assigment.dart
@@ -0,0 +1,11 @@
+void main() {
+  double age = 24;
+  age += 1; // Here age+=1 means age = age + 1.
+  print("After ADdition Age is $age");
+  age -= 1; //here age -=1 mean age = age - 1.
+  print("After aubtraction Age is $age");
+  age *= 2; //here age *=2 mean age = age * 2.
+  print("after Multiplication Age is $age");
+  age /= 2; //here age/=2 mean age = age /2.
+  print("after Divisoin Age is $age");
+}
