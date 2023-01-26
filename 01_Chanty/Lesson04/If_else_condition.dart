f54b84c03ccc4c6d68096c5a044780821b5c7792
MrrChanty <chantytouch0702@gmail.com>
2023-01-26 21:08:22 +0700

    Lesson

---
+++ 01_Chanty/Lesson04/If_else_condition.dart
@@ -0,0 +1,10 @@
+void main(List<String> args) {
+  var username = "Touch Chanty";
+  var pwd ="123";
+  if(username=="Touch Chanty" && pwd=="123"){
+    print("Welcome to our System!");
+  }else{
+    print("You are entered invalid username and password!");
+    print("Please retry again !");
+  }
+}
