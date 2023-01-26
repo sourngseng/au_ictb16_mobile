9294dce54dc61d8f4faf29185ae9a7cdf2bb28d4
MrrChanty <chantytouch0702@gmail.com>
2023-01-26 19:19:53 +0700

    Lesson03

---
+++ 01_Chanty/Lesson03/If_elseif_condition.dart
@@ -0,0 +1,20 @@
+void main(List<String> args) {
+  int noDayOfWeek = 3;
+  if(noDayOfWeek ==1){
+    print("Today is Monday !");
+  }else if(noDayOfWeek==2){
+    print("Today is Tuesday !");
+  }else if(noDayOfWeek==3){
+    print("Today is Wednesday !");
+  }else if(noDayOfWeek==4){
+    print("Today is Thursday !");
+  }else if(noDayOfWeek==5){
+    print("Today is Friday !");
+  }else if(noDayOfWeek==6){
+    print("Today is Saturday !");
+  }else if(noDayOfWeek==7){
+    print("Today is Sunday !");
+  }else {
+    print("Invalid Day, Please enter(1-7)!");
+  }
+}
